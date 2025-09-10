67e7e770cc1f240b0d94a45317765fd9671ccd5f
Tarun Asati <tarunasati96@gmail.com>
2025-09-10 23:22:12 +0530

    Time: 2052 ms (29.69%), Space: 13.4 MB (31.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,7 +6,21 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        # brute force
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j] == target :
                     return [i,j]
+
+        # # better solution
+        # i=0
+        # for j in range(i+1, len(nums)):
+        #     if nums[i]+nums[j]== target :
+        #         return [i,j]
+        #     elif nums[i]+nums[j] != target :
+        #         if j == len(nums)-1 :
+        #             i=i+1
+        #         else :
+        #             continue
+
+            
