096f22033fb249e8e73792599cdc60b2b8289a87
Tarun Asati <tarunasati96@gmail.com>
2025-09-12 10:08:46 +0530

    Time: 12 ms (29.78%), Space: 15.3 MB (5.84%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,14 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        freq={}
-        for num in nums :
-            if num in freq :
-                freq[num]+=1
-            else :
-                freq[num]=1
+        # brute force solution
+        # freq={}
+        # for num in nums :
+        #     if num in freq :
+        #         freq[num]+=1
+        #     else :
+        #         freq[num]=1
 
-        for key, value in freq.items():
+        # for key, value in freq.items():
+        #     if value==1 :
+        #         return key
+
+        # better solution
+        n=len(nums)
+        hash_list ={}
+        for i in range(0,n):
+            hash_list[nums[i]]=hash_list.get(nums[i],0)+1
+        
+        for key, value in hash_list.items():
             if value==1 :
                 return key
