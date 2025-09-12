7756fc1ebcbc0a4e31e03997ea0987de34914574
Tarun Asati <tarunasati96@gmail.com>
2025-09-12 09:57:31 +0530

    Time: 0 ms (100%), Space: 13.3 MB (89.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,10 +5,18 @@ class Solution(object):
         :rtype: int
         """
 
-        lst=[]
-        n=len(nums)
-        for i in range (0,n+1) :
-            lst.append(i)
+        # better solution
+        # lst=[]
+        # n=len(nums)
+        # for i in range (0,n+1) :
+        #     lst.append(i)
         
-        missing_num = list(set(lst)-set(nums))
-        return missing_num[0]
+        # missing_num = list(set(lst)-set(nums))
+        # return missing_num[0]
+
+        # optimal solution
+        n=len(nums)
+        expected_sum = n*(n+1)/2
+        actual_sum = sum(nums)
+        missing_no = expected_sum-actual_sum
+        return missing_no
