fd7495a9d6db322f88719650554a5bb9ae67ce46
Tarun Asati <tarunasati96@gmail.com>
2025-09-15 22:26:06 +0530

    Time: 0 ms (100%), Space: 16.6 MB (36.29%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -5,11 +5,37 @@ class Solution(object):
         :rtype: int
         """
 
-        # brute force solution
-        s=len(nums)
-        for i in range(s-1,0,-2):
-            if nums[i]!=nums[i-1]:
-                return nums[i]
-        return nums[0]
+        # # brute force solution -> O(n)
+        # s=len(nums)
+        # for i in range(s-1,0,-2):
+        #     if nums[i]!=nums[i-1]:
+        #         return nums[i]
+        # return nums[0]
             
+        # # optimal solution -> O(logn)
+        s=len(nums)
+        # handling edge cases 
+        if s==1 :
+            return nums[0]
+        if nums[0]!=nums[1] :
+            return nums[0]
+        if nums[s-1]!=nums[s-2]:
+            return nums[s-1]
 
+        # binary search approach
+        low =0
+        high = s-1
+        while low <= high :
+            mid = (high+low)//2
+            if nums[mid]!=nums[mid+1] and nums[mid]!=nums[mid-1] :
+                return nums[mid]
+
+            # odd-even index pairing -> right side
+            if (mid%2==0 and nums[mid-1]==nums[mid]) or (mid%2==1 and nums[mid]==nums[mid+1]) :
+                high=mid-1 # switch to left side
+
+            # even-odd index pairing -> left side 
+            else :
+                low =mid+1 # switch to right side 
+         
+        
