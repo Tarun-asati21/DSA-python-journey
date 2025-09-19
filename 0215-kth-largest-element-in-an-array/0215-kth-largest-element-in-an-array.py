dada8f3803f387b2cf9e3bce6dc8384c29172f84
Tarun Asati <tarunasati96@gmail.com>
2025-09-19 10:36:46 +0530

    Time: 473 ms (51.38%), Space: 21.9 MB (16.36%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -7,5 +7,14 @@ class Solution(object):
         """
         
         # brute force -> using sorting 
-        nums=sorted(nums, reverse=True)
-        return nums[k-1]
+        # nums=sorted(nums, reverse=True)
+        # return nums[k-1]
+
+        # better solution -> using maxheap
+        from heapq import heapify, heappush , heappop
+
+        nums=[-num for num in nums]
+        heapify(nums)
+        for i in range(k):
+            result = heappop(nums)
+        return -result
