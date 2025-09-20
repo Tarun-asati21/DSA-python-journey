b401aa80ba1bfb92164f3dc2eb32edfcc2825878
Tarun Asati <tarunasati96@gmail.com>
2025-09-20 12:54:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (27.53%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,27 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums =sorted(nums)
-        return nums[0]
-        # few edge cases checks :
-        # s=len(nums)
-        # if s==1 :
-        #     return nums[0]
-        # if nums[0]>nums[1]:
-        #     return nums[1]
-        # if nums[-1]<nums[-2]:
-        #     return nums[-1]
-
-        # l=0
-        # r=s-1
-        # while l<r :
-        #     m = (l+r)//2
-        #     if (nums[m]>nums[m-1] and nums[m]>nums[m+1]) :
-        #         return nums[m+1]
-        #     elif nums[m]<nums[m-1] and nums[m]<nums[m+1]:
-        #         return nums[m]
-        #     elif nums[m-1]<nums[m]<nums[m+1]:
-        #         r=m-1
-        #     else :
-        #         l=m+1
-        # return -1
+        # Binary Search -> takes smallest from the sorted part -> and ignore the sorted part
+        mini= float("inf") # -> initialises a minimum element equal to infinity
+        l=0
+        r=len(nums)-1
+        while l<=r :
+            m = (l+r)//2
+            if nums[l]<=nums[m]:
+                mini = min(mini, nums[l])
+                l=m+1
+            else : # nums[m]<nums[r]
+                mini=min(mini, nums[m])
+                r=m-1      
+        return mini
