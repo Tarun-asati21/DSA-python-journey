2eb5e00ed99cb16f203bac8615958dd5311f792b
Tarun Asati <tarunasati96@gmail.com>
2025-09-21 15:03:27 +0530

    Time: 2 ms (16.37%), Space: 12.5 MB (11.26%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
@@ -5,8 +5,23 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        count=0
-        for hr in hours :
-            if hr >= target :
-                count+=1
-        return count
+        # # brute force
+        # count=0
+        # for hr in hours :
+        #     if hr >= target :
+        #         count+=1
+        # return count
+
+        # better solution -> binary search - upper bound
+        hours_=sorted(hours)
+        mini=len(hours)
+        l=0
+        r=len(hours)-1
+        while l<=r :
+            m=(l+r)//2
+            if hours_[m] >= target:
+                mini=min(mini, m)
+                r=m-1
+            else :
+                l=m+1
+        return len(hours_[mini:])
