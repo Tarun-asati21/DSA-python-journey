c46e25640eb972b3845c5cf5b9c87b069541ea9d
Tarun Asati <tarunasati96@gmail.com>
2025-09-21 15:44:49 +0530

    Time: 15 ms (61.08%), Space: 12.5 MB (69.58%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,11 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # brute force -> linear search
+        # lst=[]
+        # for i in range(0,len(nums)):
+        #     count=0
+        #     for j in range (0, len(nums)):
+        #         if nums[j]<nums[i]:
+        #             count+=1
+        #     lst.append(count)
+        # return lst
+
+        # better solution -> binary search-lowerbound
+        sort_nums=sorted(nums)
         lst=[]
-        for i in range(0,len(nums)):
-            count=0
-            for j in range (0, len(nums)):
-                if nums[j]<nums[i]:
-                    count+=1
-            lst.append(count)
+        for num in nums :
+            mini=len(nums)
+            l=0
+            r=len(nums)-1
+            while l<=r :
+                m=(l+r)//2
+                if sort_nums[m]>=num :
+                    mini=min(mini,m)
+                    r=m-1
+                else :
+                    l=m+1
+            lst.append(mini)
         return lst
