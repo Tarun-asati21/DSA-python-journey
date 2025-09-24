4d8e18434b11230fc951f90dbd3783c13927e6a7
Tarun Asati <tarunasati96@gmail.com>
2025-09-24 10:41:23 +0530

    Time: 8 ms (63.94%), Space: 17.8 MB (43.95%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,15 +1,21 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        # brute force 
-        if n<0 :
+        # # brute force -> maine socha -> while loop pura last tak jaake divide karke check kar raha hai, har no ke liye -> tc badi
+        # if n<0 :
+        #     return False
+        # else :
+        #     while n :
+        #         if n == 1 :
+        #             return True
+        #         if (n >0 and n <1)  :
+        #             return False
+        #         n = n/3
+        #     return False
+
+        # better solution -> while loop jo no divisible by 3 hai, unnhi ke liye check kare -> search space choti -> tc choti
+        if n<=0 :
             return False
-        else :
-            while n :
-                if n == 1 :
-                    return True
-                if (n >0 and n <1)  :
-                    return False
-                n = n/3
-            return False
-    
+        while n%3 == 0:
+            n//=3
+        return n==1
     
