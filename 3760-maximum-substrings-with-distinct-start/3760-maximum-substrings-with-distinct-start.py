ddeb12f1c2c6b5f0685fe059ebb5ffc5d2edfea6
Tarun Asati <tarunasati96@gmail.com>
2026-04-03 09:42:36 +0530

    Time: 7 ms (96.15%), Space: 20 MB (48.62%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxDistinct(self, s: str) -> int:
-        sub={}
-        for ch in s :
-            if ch in sub :
-                sub[ch]+=1
-            else :
-                sub[ch]=1
-        val=0
-        for key in sub :
-            val+=1
-        return val
+        # # brute force 
+        # sub={}
+        # for ch in s :
+        #     if ch in sub :
+        #         sub[ch]+=1
+        #     else :
+        #         sub[ch]=1
+        # val=0
+        # for key in sub :
+        #     val+=1
+        # return val
+
+        # one line solution 
+        return len(set(s))
