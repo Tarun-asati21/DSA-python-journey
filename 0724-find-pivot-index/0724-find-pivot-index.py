6ff67c8b1aff4e818c8240ab18031bfe46ab4b4f
Tarun Asati <tarunasati96@gmail.com>
2026-04-14 12:36:04 +0530

    Time: 3 ms (89.34%), Space: 20.4 MB (56.95%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -6,17 +6,26 @@ class Solution:
         #         return i 
         # return -1
 
-        # optimise
-        pre = [0] * len(nums)
-        for i in range (1, len(nums)) :
-            pre[i] = nums[i-1] + pre[i-1]
+        # # better
+        # pre = [0] * len(nums)
+        # for i in range (1, len(nums)) :
+        #     pre[i] = nums[i-1] + pre[i-1]
 
-        suff = [0] * len(nums)
-        for i in range(len(nums)-2, -1, -1):
-            suff[i] = nums[i+1] +  suff[i+1]
+        # suff = [0] * len(nums)
+        # for i in range(len(nums)-2, -1, -1):
+        #     suff[i] = nums[i+1] +  suff[i+1]
         
-        for  i in range(len(nums)) :
-            if pre[i] == suff[i] :
-                return i 
-                break
-        return -1
+        # for  i in range(len(nums)) :
+        #     if pre[i] == suff[i] :
+        #         return i 
+        #         break
+        # return -1
+
+        # optimal 
+        sumi = sum(nums)
+        left = 0
+        for i in range(len(nums)) :
+            if left == sumi - left - nums[i] :
+                return i
+            left += nums[i]
+        return -1 
