e74ac8442ee29e3c686489de8d68b20be56f054b
Tarun Asati <tarunasati96@gmail.com>
2026-04-20 12:09:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (28.32%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -1,27 +1,20 @@
-class Solution:
-    def maxDistance(self, colors: List[int]) -> int:
-        # # brute force - TC = O(n**2)
-        # ans = 0 
-        # for i in range (0, len(colors)) :
-        #     for j in range (i+1, len(colors)) :
-        #         if colors[i] != colors[j] :
-        #             ans = max(ans, abs(i-j))
-        # return ans 
-
-        # TC = O(n)
-        n =len(colors)
+class Solution(object):
+    def maxDistance(self, colors):
+        """
+        :type colors: List[int]
+        :rtype: int
+        """
         ans = 0
+        n = len(colors)
 
         for i in range(n-1, 0, -1) :
-            if colors[0] != colors[i] :
-                ans = max(ans, i)
+            if colors[i] != colors[0] :
+                ans = max(ans, i) 
                 break
-        
-        for i in range(n-1) :
-            if colors[-1] != colors[i] :
-                ans = max(ans, n-1-i)
+
+        for i in range(0, n-1) :
+            if colors[i] != colors[-1] :
+                ans = max(ans, n-1-i) 
                 break
         
         return ans 
-        
-
