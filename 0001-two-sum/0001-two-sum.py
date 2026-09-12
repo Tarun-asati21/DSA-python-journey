4bb87f1baf97f0c4bc4127ddad245fde939692d5
Tarun Asati <tarunasati96@gmail.com>
2026-09-12 20:01:28 +0530

    Time: 1743 ms (16.53%), Space: 19.7 MB (82.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,7 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
-        # brute force
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target :
-                    return [i,j]
-
-        # # better solution
-        # i=0
-        # for j in range(i+1, len(nums)):
-        #     if nums[i]+nums[j]== target :
-        #         return [i,j]
-        #     elif nums[i]+nums[j] != target :
-        #         if j == len(nums)-1 :
-        #             i=i+1
-        #         else :
-        #             continue
-
-            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # extreme brute force 
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target :
+                    return[i,j]
