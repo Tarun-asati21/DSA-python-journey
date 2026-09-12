0b1044e099f55bf72c1fe6c5492c12b6c48ab7d7
Tarun Asati <tarunasati96@gmail.com>
2026-09-12 20:22:15 +0530

    Time: 11 ms (77.48%), Space: 19.4 MB (46.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,19 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        from collections import defaultdict
-        hashmap=defaultdict(list)
-
-        lst=[s,t]
-        for ch in lst :
-            sorted_str= tuple(sorted(ch))
-            hashmap[sorted_str].append(ch)
-        
-        if len(hashmap)==1:
-            return True
-        else:
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) :
             return False
+        
+        freq = {}
+        for i in range(len(s)) :
+            freq[s[i]] = freq.get(s[i],0) + 1
+            freq[t[i]] = freq.get(t[i],0) - 1
+        
+        for value in freq.values():
+            if value != 0 :
+                return False
+        return True
+        
+
+
+
+        
