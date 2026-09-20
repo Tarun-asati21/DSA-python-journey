dd63b1ed5292b50e7892ecd0db101f5a8d7f8e52
Tarun Asati <tarunasati96@gmail.com>
2026-09-21 00:05:11 +0530

    Time: 39 ms (25.97%), Space: 52.8 MB (15.5%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,10 +1,25 @@
 class Solution:
     def findCenter(self, edges: list[list[int]]) -> int:
         # array brute force coming to my mind is at first instant :
-        arr = edges[0]  + edges[1]
-        freq={}
-        for num in arr :
-            freq[num] = freq.get(num,0)+1
-        for k,v in freq.items() :
-            if v == 2 :
+        # arr = edges[0]  + edges[1]
+        # freq={}
+        # for num in arr :
+        #     freq[num] = freq.get(num,0)+1
+        # for k,v in freq.items() :
+        #     if v == 2 :
+        #         return k
+
+        # using graph theory - degree concept :
+        # the center node since is connected to every other node :  N-1 edges , ie degree = N-1
+        degree = {}
+        for edge in edges :
+            node1 = edge[0]
+            node2 = edge[1]
+            degree[node1] = degree.get(node1,0)+1
+            degree[node2] = degree.get(node2,0)+1
+        
+        for k,v in degree.items():
+            if v == len(edges) :
                 return k
+
+
