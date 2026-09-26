3cb56768e920ff8b8bd0d4c3bd218b75f626adaf
Tarun Asati <tarunasati96@gmail.com>
2026-09-26 14:22:33 +0530

    Time: 371 ms (60.32%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,12 @@
+WITH CTE AS (
+    SELECT *
+    FROM Employees
+    WHERE salary < 30000
+)
+SELECT CTE.employee_id
+FROM CTE
+WHERE CTE.manager_id NOT IN (
+    SELECT employee_id
+    FROM Employees
+)
+ORDER BY CTE.employee_id
