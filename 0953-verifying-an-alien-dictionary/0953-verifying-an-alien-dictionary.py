f8dad5d56197189eb954bd05b171ddd470a53572
Tarun Asati <tarunasati96@gmail.com>
2026-09-26 19:49:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.26%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAlienSorted(self, words: list[str], order: str) -> bool:
+        order_freq={j:i for i, j in enumerate(order)}
+
+        for i in range(len(words)-1):
+            w1, w2 = words[i], words[i+1]
+
+            for j in range(len(w1)):
+                if j == len(w2):
+                    return False
+                if w1[j] != w2[j] :
+                    if order_freq[w1[j]] > order_freq[w2[j]] :
+                        return False
+                    break
+        
+        return True
+                
